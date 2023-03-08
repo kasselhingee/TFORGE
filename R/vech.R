b6b8a41cf3f1ac3ac4b7e466adf0db17c7a27ecc
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-03-08 14:28:26 +1100

    vech implemented

---
+++ R/vech.R
@@ -0,0 +1,21 @@
+#' @title Stack elements of a symmetric matrix into a vector.
+#' @description
+#' The `vech` operator as defined by \insertCite{@Section 3.8 @magnus2019ma}{PivotalBootstrapMatrixData}.
+#' @details
+#' Columns are stacked in order from left to right, entries above the diagonal are ignored.
+#' Matrix is not checked for symmetry.
+#' `vech` uses [`lower.tri()`].
+#' `invvech` is the opposite of `vech`. It could be much more efficient with memory and operations.
+vech <- function(m){
+  m[lower.tri(m, diag = TRUE)]
+}
+
+invvech <- function(x,...){
+  # n is s.t. l = 0.5 n (n+1) --> n = 0.5(-1 + sqrt(4l+1))
+  n <- (-1 + sqrt(8*length(x) + 1))/2
+  m <- matrix(NA, nrow = n, ncol = n)
+  m[lower.tri(m, diag = TRUE)] <- x
+  m[upper.tri(m)] <- t(m)[upper.tri(m)]
+  return(m)
+}
+
---
+++ tests/testthat/test-vech.R
@@ -0,0 +1,8 @@
+test_that("vech() works", {
+  expect_equal(vech(matrix(1:9, byrow = FALSE, nrow = 3)),
+               c(1:3, 5:6, 9))
+})
+test_that("invvech() works", {
+  expect_equal(invvec(c(1:3, 5:6, 9)),
+               matrix(c(1,2,3,2,5,6,3,6,9), byrow = FALSE, nrow = 3))
+})
