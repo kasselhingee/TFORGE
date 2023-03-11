b1b8b1e665bebaae061727d537fecde429227518
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-03-11 14:04:03 +1100

    rsymm is from Gaussian instead

--- R/rsymm.R
+++ R/rsymm.R
@@ -1,11 +1,11 @@
 #' @title Simulate Symmetric Matrices
 #' @description
-#' Simulate symmetric matrices with elements iid from a uniform distributions centred on zero.
+#' Simulate symmetric matrices with elements iid from a Normal distribution centred on zero.
 #' @param n Number of matrices
 #' @param p Number of rows and columns of the matrices
-#' @param s Width of the uniform distribution.
+#' @param s The standard deviation(s) of the elements.
 #' @return A list of matrices.
 #' @export
-rsymm <- function(n, p, s = 1){
-  replicate(n, invvech(runif(p*(p+1)/2, min = -s, max = s)), simplify = FALSE)
+rsymm <- function(n, p, sd = 1){
+  replicate(n, invvech(rnorm(p*(p+1)/2, sd = sd)), simplify = FALSE)
 }
