e23be19731afbd8e6551d5f4381e79d664082772
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-04-06 16:00:29 +1000

    drafted Schwartzmann

---
+++ R/Schwartzmann.R
@@ -0,0 +1,87 @@
+
+# Schwartzmann vecd (which is not quite vech and has a different ordering)
+vecd <- function(m){
+  c(diag(m), sqrt(2) * m[lower.tri(m, diag = FALSE)])
+}
+
+S_mcovar <- function(merr){
+  if(is.array(merr)){if (length(dim(merr))==3){
+    merr <- lapply(1:dim(merr)[3], function(i) merr[,,i])
+  }}
+  mcovarls <- lapply(merr, function(m){
+    x <- vecd(m)
+    x %*% t(x)
+  })
+  n <- length(merr)
+  mmean(mcovarls) * n / (n-1)
+}
+
+# i is the index that correponds to 1, p is the number of rows/columns
+Eii <- function(i, p){
+  out <- matrix(0, p, p)
+  out[i, i] <- 1
+  return(out)
+}
+
+#make a block diagonal matrix from A and B
+blockdiag <- function(A, B){
+  out <- matrix(0, nrow(A) + nrow(B), ncol(A) + ncol(B))
+  rA <- nrow(A)
+  cA <- ncol(A)
+  out[1:rA, 1:cA] <- A
+  out[rA + (1:nrow(B)), cA + (1:ncol(B))] <- B
+  return(out)
+}
+
+#Schwartzmann's wii from eqn 16
+wii <- function(i, U1, U2){
+  Emat <- Eii(i, nrow(U1))
+  c(
+    vecd(U1 %*% Emat %*% t(U1)),
+    -vecd(U2 %*% Emat %*% t(U2))
+  )
+}
+
+#Schwartzmann's eigenvalue Omega(M) (eq 16)
+#' @param n1 Sample size of 1
+#' @param n2 Sample size of 2
+#' @param U1 Eigenvectors for mean of 1
+#' @param U2 Eigenvectors for mean of 2
+Omega_eval <- function(n1, n2, U1, U2){
+  vals <- lapply(1:nrow(U1), function(i){
+    w <- wii(i, U1, U2)
+    w %*% t(w)
+  })
+  msum(vals) * n1 * n2 / (n1 + n2)
+}
+
+#compute estimates Schwartzmann's a and v for the test statistic distribution
+#ms1 and ms2 are the two samples
+S_anv <- function(ms1, ms2){
+  cov1 <- S_mcovar(merr(ms1))
+  cov2 <- S_mcovar(merr(ms2))
+  Sigma <- blockdiag(cov1, cov2)
+  
+  es1 <- eigen(mmean(ms1))
+  es2 <- eigen(mmean(ms2))
+  Omega <- Omega_eval(length(ms1), length(ms2), es1$vectors, es2$vectors)
+  
+  tr_soso <- sum(diag(Sigma %*% Omega %*% Sigma %*% Omega))
+  tr_so <- sum(diag(Sigma %*% Omega))
+  a <- tr_soso/tr_so
+  v <- tr_so^2/tr_soso
+  return(list(
+    a = a,
+    v = v
+  ))
+}
+
+stat_schwartzmann_eval <- function(ms1, ms2){
+  n1 <- length(ms1)
+  n2 <- length(ms2)
+  L1 <- eigen(mmean(ms1))$values
+  L2 <- eigen(mmean(ms2))$values
+  sum((L1 - L2)^2) * n1 * n2 / (n1 + n2)
+}
+
+
---
+++ tests/testthat/test-Schwartzmann.R
@@ -0,0 +1,22 @@
+test_that("vecd() works", {
+  expect_equal(vecd(matrix(1:9, byrow = FALSE, nrow = 3)),
+               c(1, 5, 9, sqrt(2) * c(2,3,6)))
+})
+
+test_that("S_mcovar gets close to true", {
+  skip("slow")
+  Ysample <- rsymm(100000, matrix(0, 3, 3), sigma = diag( 3 * (3+1)/2))
+  expect_silent(c0 <- S_mcovar(merr(Ysample)))
+  expect_equal(c0, diag(c(rep(1, 3), rep(2, 3))), tolerance = 0.06)
+})
+
+test_that("makeblockdiagonal works", {
+  A <- matrix(1, 3, 5)
+  B <- matrix(2, 4, 6)
+  expect_equal(blockdiag(A, B)[4,1], 0)
+  expect_equal(blockdiag(A, B)[4,6], 2)
+})
+
+test_that("Distribution is exact for 'OI' covariances", {
+  
+})
