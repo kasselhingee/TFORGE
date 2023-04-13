763bd64ab30efe972b132e92b1aebf7d6240a76c
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-04-13 11:47:42 +1000

    test of p-value distribution failing

--- tests/testthat/test-Schwartzmann.R
+++ tests/testthat/test-Schwartzmann.R
@@ -17,6 +17,35 @@ test_that("makeblockdiagonal works", {
   expect_equal(blockdiag(A, B)[4,6], 2)
 })
 
+test_that("Results are consistent with the simulation check at the start of Schwartzmann Section 3.1.", {
+  p <- 3
+  C2 <- C1 <- diag(p*(p+1)/2)
+  
+  # set up distribution means
+  M0 <- diag(c(1,2,4))
+ 
+  Sigma <- rWishart(1, 6, diag(6)) 
+  # simulate test statistic
+  simulateTstat <- function(n1, n2){
+    ms1 <- rsymm(n1, mn1, C1)
+    ms2 <- rsymm(n2, mn2, C2)
+    res <- stat_schwartzmann_eval(ms1, ms2)
+    return(res$t)
+  }
+  simts <- replicate(100, simulateTstat(50, 100))
+
+  #map from vech back to a matrix and then forward with vecd
+  #vecd(invvech(1:6))
+  #vecd(invvech(diag(C1)))
+  
+  # approximate distribution parameters
+  anv <- S_anv(50, 100, mn1, mn2, 
+               C1 = diag(vecd(invvech(diag(C1)))), 
+               C2 = diag(vecd(invvech(diag(C1))))) #the extra work accounts for strange vecd of Schwartzmann
+  qqplot(simts/anv$a,
+    qchisq(ppoints(100), df = anv$v))
+})
+
 test_that("S_anv() gives exact distribution for 'OI' covariances", {
   p <- 3
   C2 <- C1 <- diag(p*(p+1)/2)
@@ -34,13 +63,8 @@ test_that("S_anv() gives exact distribution for 'OI' covariances", {
     ms1 <- rsymm(n1, mn1, C1)
     ms2 <- rsymm(n2, mn2, C2)
     res <- stat_schwartzmann_eval(ms1, ms2)
-    return(res$t)
+    return(res$p)
   }
-  simts <- replicate(100, simulateTstat(50, 100))
-  
-  # approximate distribution parameters
-  anv <- S_anv(n1, n2, mn1, mn2, 
-               C1 = C1, #missing something with the sqrt2
-               C2 = C2) #missing something with the sqrt2
-  qqline(simts / anv$a, qchisq, v = anv$v)
+  simts <- replicate(1000, simulateTstat(50, 100))
+  plot(ecdf(simts))  #pvalue distribution should be approximately uniform, but it isn't
 })
