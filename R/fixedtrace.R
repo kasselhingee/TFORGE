85bff4df593d180bdc2c0ae0ecd8c6848ac900e0
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-08-21 12:09:34 +1000

    fixedtrace test has a maxit argument

--- R/fixedtrace.R
+++ R/fixedtrace.R
@@ -87,8 +87,9 @@ stat_fixedtrace <- function(x, evals = NULL, evecs = NULL, NAonerror = FALSE){
  
 #' @describeIn stat_fixedtrace Bootstrap test.
 #' @param B The number of bootstrap samples
+#' @param maxit The maximum number of iterations to use in finding the weights. Passed to `[emplik::el.test()]`.
 #' @export
-test_ss_fixedtrace <- function(ms, evals, B, evecs = NULL){
+test_ss_fixedtrace <- function(ms, evals, B, evecs = NULL, maxit = 25){
   evals <- sort(evals, decreasing = TRUE)
   if (!isTRUE(all.equal(sum(evals), sum(diag(ms[[1]]))))){
     warning("Provided evals do not sum to trace of average.")
@@ -101,7 +102,9 @@ test_ss_fixedtrace <- function(ms, evals, B, evecs = NULL){
     nullmean <- evecs %*% diag(evals) %*% t(evecs)
   }
   
-  elres <- emplik::el.test(do.call(rbind, lapply(ms, vech)), vech(nullmean))
+  warning("need to check maxit for el.test()")
+  elres <- emplik::el.test(do.call(rbind, lapply(ms, vech)), vech(nullmean), maxit = maxit)
+  if (elres$nits == maxit){warning("Reached maximum iterations in el.test() at best null mean.")}
   if (abs(length(ms) - sum(elres$wts)) > 1E-2){
     # above sees if weight sums to n (otherwise should sum to k < n being number of points in face). Assume proposed mean is close or outside convex hull and with pval of zero, t0 of +infty
     return(list(
@@ -122,7 +125,7 @@ test_ss_fixedtrace <- function(ms, evals, B, evecs = NULL){
 
 #' @describeIn stat_fixedtrace Bootstrap test.
 #' @export
-test_ms_fixedtrace <- function(mss, B){
+test_ms_fixedtrace <- function(mss, B, maxit = 25){
   mss <- as.mstorsst(mss)
   t0 <- stat_fixedtrace(mss, NAonerror = FALSE)
   evals <- attr(t0, "null_evals")
@@ -136,7 +139,8 @@ test_ms_fixedtrace <- function(mss, B){
   
   # compute corresponding weights that lead to emp.lik.
   wts <- mapply(function(ms, nullmean){
-    elres <- emplik::el.test(do.call(rbind, lapply(ms, vech)), vech(nullmean))
+    elres <- emplik::el.test(do.call(rbind, lapply(ms, vech)), vech(nullmean), maxit = maxit)
+  if (elres$nits == maxit){warning("Reached maximum iterations in el.test() at best null mean.")}
     elres$wts
   }, ms = mss, nullmean = nullmeans, SIMPLIFY = FALSE)
 
