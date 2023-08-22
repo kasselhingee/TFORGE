759ca0edbd74b05662ddd9363913197711ab0d34
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-08-22 19:18:40 +1000

    ss1fixedtrace still doesn't have the correct distribution

--- tests/testthat/test-ss1fixedtrace.R
+++ tests/testthat/test-ss1fixedtrace.R
@@ -1,8 +1,8 @@
 test_that("stat_ss1fixedtrace() on single sample from NULL is consistent with chisq", {
-  set.seed(130)
-  vals <- replicate(100, {
-    Y <- rsymm_norm(30, diag(c(3,2,1)))
-    Y <- lapply(Y, function(m) {m[1,1] <- - sum(diag(m)[-1]); return(m)})
+  set.seed(1311)
+  vals <- pbreplicate(2000, {
+    Y <- rsymm_norm(30, diag(c(1, 0, -1)))
+    Y <- lapply(Y, function(m) {diag(m) <- diag(m) - drop(diag(m) %*% rep(1/sqrt(3), 3)) * rep(1/sqrt(3), 3); return(m)})
     Y <- lapply(Y, function(m) { #replace eigenvalues with normalised ones
         evecs <- eigen(m)$vectors
         evals <- eigen(m)$values
@@ -14,9 +14,9 @@ test_that("stat_ss1fixedtrace() on single sample from NULL is consistent with ch
     # hasfixedtrace(Y, tolerance = 1E10 * sqrt(.Machine$double.eps))
     # hasss1(Y)
     stat_ss1fixedtrace(Y, evals = c(1, 0, -1))
-    })
+    }, cl = 2)
   
   qqplot(vals, y = rchisq(1E6, df = 1))
-  res <- ks.test(vals, "pchisq", df = 3)
+  res <- ks.test(vals, "pchisq", df = 1)
   expect_gt(res$p.value, 0.2)
 })
