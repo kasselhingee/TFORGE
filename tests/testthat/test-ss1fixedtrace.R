51dae896e58c617ca7047ff097101032a64f217a
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-08-23 10:24:14 +1000

    single sample stat_ss1fixedtrace() has correct NULL distribution :)

--- tests/testthat/test-ss1fixedtrace.R
+++ tests/testthat/test-ss1fixedtrace.R
@@ -1,9 +1,9 @@
 test_that("stat_ss1fixedtrace() on single sample from NULL is consistent with chisq", {
   set.seed(1311)
-  vals <- pbreplicate(2000, {
-    Y <- rsymm_norm(30, diag(c(1, 0, -1)))
-    Y <- lapply(Y, function(m) {diag(m) <- diag(m) - drop(diag(m) %*% rep(1/sqrt(3), 3)) * rep(1/sqrt(3), 3); return(m)})
-    Y <- lapply(Y, function(m) { #replace eigenvalues with normalised ones
+  vals <- pbreplicate(1000, {
+    Y <- rsymm_norm(50, diag(c(1/sqrt(2), 0, -1/sqrt(2))))
+    Y <- lapply(Y, function(m) {diag(m) <- diag(m) - drop(diag(m) %*% rep(1/sqrt(3), 3)) * rep(1/sqrt(3), 3); return(m)}) #this shifts the distribution if the trace from rsymm_norm isn't symmertic about zero
+    Y <- lapply(Y, function(m) { #replace eigenvalues with normalised ones. This changes the distribution, but I think it is symmetric about the mean normalised eigenvalues - just like averages of directions.
         evecs <- eigen(m)$vectors
         evals <- eigen(m)$values
         evals <- evals/sqrt(sum(evals^2))
@@ -13,7 +13,7 @@ test_that("stat_ss1fixedtrace() on single sample from NULL is consistent with ch
     })
     # hasfixedtrace(Y, tolerance = 1E10 * sqrt(.Machine$double.eps))
     # hasss1(Y)
-    stat_ss1fixedtrace(Y, evals = c(1, 0, -1))
+    stat_ss1fixedtrace(Y, evals = c(1/sqrt(2), 0, -1/sqrt(2)))
     }, cl = 2)
   
   qqplot(vals, y = rchisq(1E6, df = 1))
