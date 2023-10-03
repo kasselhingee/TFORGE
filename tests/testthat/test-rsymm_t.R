a35add70803cb6a42e9561a2f711eaecd059df3c
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-10-04 07:29:18 +1100

    rsymm_t test used to use abind()

--- tests/testthat/test-rsymm_t.R
+++ tests/testthat/test-rsymm_t.R
@@ -1,7 +1,7 @@
 test_that("rsymm_t has correct mean", {
   set.seed(3654)
   Ys <- rsymm_t(100, delta = matrix(1, nrow = 3, ncol = 3), df = 10, sigma = diag(c(3,2,1,1,1,1)))
-  arr <- abind::abind(Ys, along = 3)
+  arr <- simplify2array(Ys)
   mn <- apply(arr, MARGIN = c(1, 2), mean) #close to 1 - yes :)
   expect_lt(max(abs(mn - 1)), 2E-1)
   # do.call(rbind, lapply(Ys, vech, name = TRUE)) |>
