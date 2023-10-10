be61d3625d3c9c05cd179ed589d69bb0e4ea0a0c
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-10-10 18:50:09 +1100

    checking RcppArmadillo cov_evals

---
+++ tests/testthat/test-cov_evals.R
@@ -0,0 +1,9 @@
+test_that("cov_evals_inside() matches cov_evals_inside_cpp()",{
+  set.seed(12)
+  Ysample <- as.sst(rsymm(1E2, diag(c(3,2,1))))
+  mcov <- mcovar(Ysample)
+  evecs <- eigen_desc(mmean(Ysample))$vectors
+  dupmat <- dup(nrow(evecs))
+  expect_equal(cov_evals_inside(evecs[, 1], evecs[, 2], dupmat, mcov), 
+    cov_evals_inside_cpp(evecs[, 1], evecs[, 2], dupmat, mcov))
+})
