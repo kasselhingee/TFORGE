a874b8d124ede1b406b5703edd6781e1300b153b
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-10-12 09:06:27 +1100

    rsymm t using Lin 1972 representation

--- R/rsymm_t.R
+++ R/rsymm_t.R
@@ -6,13 +6,22 @@
 #' @param sigma The scale paramater matrix for the elements arranged by [`vech()`]. `sigma` is passed to [`mvtnorm::rmvt()`] without any transformation.
 #' @param df Degrees of freedom
 #' @return A list of matrices.
-#' @details The `delta` is vectorised (with [`vech()`]) and then [`mvtnorm::rmvt()`] is called with `type = "shifted"`
+#' @details Using the use Lin 1972 representation A to simulate about mean of zero. Then shift all simulated values by delta. The covariance of the result is sigma *  df / (df - 2).
 #' @examples 
 #' Ys <- rsymm_t(100, delta = matrix(1, nrow = 3, ncol = 3), df = 10, sigma = diag(c(3,2,1,1,1,1)))
 #' @export
 rsymm_t <- function(n, delta, df = 1, sigma = diag(length(vech(delta)))){
   stopifnot(isSymmetric(delta))
-  tmp <- mvtnorm::rmvt(n, delta = vech(delta), df = df, sigma = sigma)
-  class(tmp) <- c("sst", class(tmp))
-  return(tmp)
+  delta <- vech(delta, name = TRUE)
+  zeros <- rep(0, length(delta))
+  nuS2 <- rchisq(n, df)
+  S2 <- nuS2/df
+  sim_l <- lapply(S2, function(x){
+    mvtnorm::rmvnorm(n = 1, mean = zeros, sigma = sigma / x)
+  })
+  out <- do.call(rbind, sim_l)
+  out <- t(t(out) + delta)
+  colnames(out) <- names(mean)
+  class(out) <- c("sst", class(out))
+  return(out)
 }
--- tests/testthat/test-rsymm_t.R
+++ tests/testthat/test-rsymm_t.R
@@ -1,9 +1,9 @@
 test_that("rsymm_t has correct mean", {
   set.seed(3654)
-  Ys <- rsymm_t(100, delta = matrix(1, nrow = 3, ncol = 3), df = 10, sigma = diag(c(3,2,1,1,1,1)))
-  arr <- simplify2array(Ys)
-  mn <- apply(arr, MARGIN = c(1, 2), mean) #close to 1 - yes :)
-  expect_lt(max(abs(mn - 1)), 2E-1)
+  sigma <- diag(c(3,2,1,1,1,1))
+  Ys <- rsymm_t(1E4, delta = matrix(1, nrow = 3, ncol = 3), df = 10, sigma = sigma)
+  expect_lt(max(abs(colMeans(Ys) - 1)), 2E-1)
+  expect_equal(cov(Ys), sigma * 10/(10-2), 1E-1)
   # do.call(rbind, lapply(Ys, vech, name = TRUE)) |>
   #   data.frame() |>
   #   tidyr::pivot_longer(everything()) |>
