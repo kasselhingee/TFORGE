96954cde71c79d27a553591c1892102b2a79bda6
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-10-24 11:47:22 +1100

    theoretical cov_evals() function

--- R/cov_evals.R
+++ R/cov_evals.R
@@ -1,21 +1,11 @@
-#' General function for getting covariance of eigenvalues from a single sample
-#' @param evecs If supplied these eigenvectors will be used instead of estimated eigenvectors. Each column is an eigenvector.
-#' @param ms A single sample (list of matrices)
-#' @param av To save computation, `av` can be passed if it is already computed.
-#' @details Computes equation (11) of `tensors_4` with \eqn{C_0} replaced with the sample analogue. If `evecs` is not provided then the eigenvectors \eqn{q_{0i}} are replaced with the eigenvectors of the average of `ms`.
-#' @return An estimated covariance matrix for the eigenvalues of `ms`.
+#' General function for computing covariance of eigenvalues.
+#' @param evecs Matrix of eigenvectors of the mean as columns.
+#' @param mcov Covariance of `vech(Y)`, where `Y` is the random matrix.
+#' @details Computes equation (11) of `tensors_4` with \eqn{C_0}.
 #' @useDynLib PivotalBootstrapMatrixData, .registration=TRUE
 #' @importFrom Rcpp evalCpp
 #' @export
-cov_evals_est <- function(ms, evecs = NULL, av = NULL){
-  ms <- as.mstorsst(ms)
-  stopifnot(inherits(ms, "sst"))
-  if (is.null(av)){av <- mmean(ms)}
-  if (is.null(evecs)){
-    evecs <- eigen_desc(av, symmetric = TRUE)$vectors
-  }
-  mcov <- mcovar(merr(ms, mean = av))
-
+cov_evals <- function(evecs, mcov){
   # the V1 / V2 matrix for a single sample depending on whether evecs estimated or supplied
   indx <- rbind(t(combn(1:nrow(evecs), 2)), #this avoids repeating elements that are symmetric
                 cbind(1:nrow(evecs), 1:nrow(evecs)))
@@ -28,6 +18,23 @@ cov_evals_est <- function(ms, evecs = NULL, av = NULL){
   V[as.matrix(indx)] <- vals
   V[lower.tri(V)] <- t(V)[lower.tri(V)]
   return(V)
+}
+
+
+# @param ms A single sample (list of matrices)
+# @param av To save computation, `av` can be passed if it is already computed.
+# @details If `evecs` is not provided then the eigenvectors \eqn{q_{0i}} are replaced with the eigenvectors of the average of `ms`.
+# @return An estimated covariance matrix for the eigenvalues of `ms`.
+cov_evals_est <- function(ms, evecs = NULL, av = NULL){
+  ms <- as.mstorsst(ms)
+  stopifnot(inherits(ms, "sst"))
+  if (is.null(av)){av <- mmean(ms)}
+  if (is.null(evecs)){
+    evecs <- eigen_desc(av, symmetric = TRUE)$vectors
+  }
+  mcov <- mcovar(merr(ms, mean = av))
+
+  cov_evals(evecs, mcov)
 }
 
 cov_evals_inside <- function(vecj, veck, dupmat, mcov){
