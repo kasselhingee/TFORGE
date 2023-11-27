d459bf08c80b7641c7b815c540982c589a6659fa
Kassel Hingee <kassel.hingee@anu.edu.au>
2023-11-27 18:13:55 +1100

    the CI satisfies the correct property that the population mean is in the region 95% of the time, but it doesn't represent the plausible spread of mean values at all

--- tests/testthat/test-fixedtrace_CI.R
+++ tests/testthat/test-fixedtrace_CI.R
@@ -15,12 +15,23 @@ test_that("conf_fixedtrace() warns when ordered boundary is intersected", {
   ms <- normtrace(ms)
   expect_warning(cr <- conf_fixedtrace(ms, alpha = 0.05, B = 1000, npts = 1000),
                  "boundary")
-  expect_true(cr$inregion(c(4,2,1)/7))
-  expect_false(cr$inregion(c(0.8, -0.1, 0.3)))
-
-  plot(cr$boundary[, 1], cr$boundary[, 2])
+})
+test_that("conf_fixedtrace() boundary plots nicely", {
+  set.seed(345)
+  ms <- rsymm_norm(n = 50, mean = diag(c(0.5,0.3,0.2)), sigma = diag(0.02, 6))
+  ms <- normtrace(ms)
+  cr <- conf_fixedtrace(ms, alpha = 0.05, B = 1000, npts = 1000)
+  points(cr$boundary[, 1], cr$boundary[, 2])
   abline(a = 0, b = 1)
   abline(a = 1/2, b = -1/2)
+  samplemeanevals <- t(replicate(100, {
+    ms <- rsymm_norm(n = 50, mean = diag(c(4,2,1)))
+    ms <- normtrace(ms)
+    evals <- eigen_desc(mmean(ms))$values
+  }))
+  points(cr$est[1], cr$est[2], pch = 2)
+  points(samplemeanevals[, 1], samplemeanevals[, 2], pch = 3)
+  plot(samplemeanevals[, 1], samplemeanevals[, 2], pch = 3)
 })
 
 test_that("conf_fixedtrace() contains population mean about 95% of the time", {
@@ -30,6 +41,6 @@ test_that("conf_fixedtrace() contains population mean about 95% of the time", {
      ms <- normtrace(ms)
      cr <- suppressWarnings(conf_fixedtrace(ms, alpha = 0.05, B = 1000, npts = 1000))
      cr$inregion(c(4,2,1)/7)})
-  expect_equal(mean(popmeanincr), 0.95, tolerance = 1E-2)
+  expect_equal(mean(popmeanincr), 0.95, tolerance = 5E-2)
 })
 
