d8f0d375bc4c7a5710422300f3be0d0034fc67ce
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-01-25 16:52:34 +1100

    stclasses test updated

--- tests/testthat/test-stclasses.R
+++ tests/testthat/test-stclasses.R
@@ -8,18 +8,19 @@ test_that("as.mstorsst()",{
   expect_silent(as.mstorsst(Ysamples[1]))
   
   expect_error(as.sst(Ysamples))
-  expect_error(as.sst(matrix(1, 2, 5)), "number")
+  expect_error(as.sst(matrix(1, 2, 5)), "Number")
 })
 
-test_that("new generics work for sst", {
+test_that("matrix generics work for sst", {
   set.seed(134)
-  Ysample <- rsymm_norm(50, diag(c(3,2,1)))
+  Ysample <- matrix(runif(6*5), nrow = 5)
   Ysample_sst <- as.sst(Ysample)
   expect_equal(Ysample_sst[1], Ysample[1])
   expect_equal(Ysample_sst[1:3], Ysample[1:3])
+  expect_equal(Ysample_sst[1,3], Ysample[1,3])
   boolindex <- runif(50) < 0.3
   expect_equal(Ysample_sst[boolindex], Ysample[boolindex])
   
-  expect_equal(Ysample_sst[[1]], Ysample[[1]])
-  expect_equal(Ysample_sst[[1:2]], Ysample[[1:2]])
+  expect_equal(Ysample_sst[[3]], Ysample[[3]])
+  expect_error(Ysample_sst[[1:2]], "more than one")
 })
