c1af09d0741dfdd4a4d17ecedd8c560deb7a23c4
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-06-18 11:47:56 +1000

    slight polish to description

---
+++ R/TFORGE-package.R
@@ -0,0 +1,12 @@
+
+#' @details
+
+
+#' # Acknowledgements
+#' Colleagues Andrew T. A. Wood and Janice Scealy played crucial roles in developing the statistical concepts and theory.
+#' 
+#' This package on Ngunnawal and Ngambri Country. We thank the Country for its influence.
+#' @references
+#' \insertAllCited{}
+"_PACKAGE"
+
