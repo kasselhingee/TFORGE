b25875df53a27e037f7e609b2d6322deed61d02f
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-06-18 12:24:36 +1000

    intro tweaks

--- R/TFORGE-package.R
+++ R/TFORGE-package.R
@@ -1,21 +1,21 @@
 #' @details
 #' Functions for conducting hypothesis tests start with `test_`.
 #' The following can conduct single sample and \eqn{k}-sample tests of eigenvalues using pivotal bootstap methods:
-#' + `test_unconstrained()`
-#' + `test_fixedtrace()` when matrices have fixed trace
-#' + `test_ss1()` when the squared eigenvalues of each matrix sums to \eqn{1}.
-#' + `test_ss1fixedtrace()` when the squared eigenvalues of each matrix sums to \eqn{1} and the trace is zero.
+#' + [`test_unconstrained()`]
+#' + [`test_fixedtrace()`] when matrices have fixed trace
+#' + [`test_ss1()`] when the squared eigenvalues of each matrix sums to \eqn{1}.
+#' + [`test_ss1fixedtrace()`] when the squared eigenvalues of each matrix sums to \eqn{1} and the trace is zero.
 #' For single sample tests the null hypothesis is a user-provided set of eigenvalues for the population mean.
 #' For \eqn{k}-sample tests the null hypothesis is that the eigenvalues of the population means are equal.
 #'
-#' Additionally `test_unconstrained_aGOE()` can perform \eqn{2}-sample tests using an approximation of the Gaussian Orthogonal Ensemble \insertCite{schwartzman2010gr}{TFORGE}.
+#' Additionally [`test_unconstrained_aGOE()`] can perform \eqn{2}-sample tests using an approximation of the Gaussian Orthogonal Ensemble \insertCite{schwartzman2010gr}{TFORGE}.
 #'
-#' To test the eigenvalue-multiplicity of the population mean of a single sample use `test_multiplicity()` (for unconstrained or fixed trace matrices).
-#' A test of the same hypothesis that requires orthogonally-invariant covariance is also available through `test_multiplicity_OI()` \insertCite{schwartzman2008in}{TFORGE}.
+#' Use [`test_multiplicity()`] to test the eigenvalue-multiplicity of the population mean of a single sample (for unconstrained or fixed trace matrices).
+#' A test of the same hypothesis that requires orthogonally-invariant covariance is also available through [`test_multiplicity_OI()`] \insertCite{schwartzman2008in}{TFORGE}.
 #' 
 #' In this package matrices within the same sample are considered independently and identically distributed.
 #' Matrices are stored in their flattened form according to [`vech()`]. See [`fsm`] for details.
-#' Samples may be provided as lists of matrices, or in their flattened from so long as the column order matches that of [`vech()`].
+#' Samples may be provided as lists of matrices or in their flattened from so long as the column order matches that of [`vech()`].
 
 #' # Acknowledgements
 #' Colleagues Andrew T. A. Wood and Janice Scealy played crucial roles in developing the statistical concepts and theory.
