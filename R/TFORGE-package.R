6264ea142b2b729153c79b376152c2b48322015b
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-06-18 11:56:47 +1000

    referencing updated

--- R/TFORGE-package.R
+++ R/TFORGE-package.R
@@ -1,9 +1,9 @@
-
 #' @details
-
 
 #' # Acknowledgements
 #' Colleagues Andrew T. A. Wood and Janice Scealy played crucial roles in developing the statistical concepts and theory.
+#'
+#' Bootstrap resampling when the sum of squared eigenvalues is constrained uses empirical likelihood weights, which are calculated by R-code written by \insertCite{owen:2013}{TFORGE}.
 #' 
 #' This package on Ngunnawal and Ngambri Country. We thank the Country for its influence.
 #' @references
