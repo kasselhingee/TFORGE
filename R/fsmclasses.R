621fd938db39cc80616595310d32278159d24f1e
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-06-19 14:43:37 +1000

    experimenting with TFORGE_fsm kfsm not being labelled with matrix class because shouldn't do matrix multiplication on them

--- R/fsmclasses.R
+++ R/fsmclasses.R
@@ -1,11 +1,12 @@
 #' @title Flat storage of symmetric matrices
 #' @name fsm
 #' @description
-#' The `fsm` class, short for 'Flat Symmetric Matrices' is for a collection symmetric matrices with each matrix stored as a row according to [`vech()`].
-#' The `fsm` class is itself a thin wrapper of the matrix class.
+#' The `TFORGE_fsm` class, short for 'Flat Symmetric Matrices', is for storing a collection of symmetric matrices with each matrix stored as a row according to [`vech()`].
+#' The `TFORGE_fsm` class is itself a thin wrapper of the array class.
 #' So, for example, `x[1, ]` will return the flattened-form of the first matrix in the collection, and `invvech(x[1,])` will be the first matrix in non-flat form.
-#'
-#' The `kfsm` class is for a collection of multiple `fsm`.
+#' The `TFORGE_kfsm` class is for a collection of multiple `TFORGE_fsm`.
+#' The function `as_flat()` automatically converts data to either `TFORGE_kfsm` or `TFORGE_fsm`.
+
 #' @examples
 #' x <- list(list(matrix(c(1,2,3,2,4,5,3,5,6), 3), 
 #'                matrix(c(2,3,4,3,5,6,4,6,7), 3)),
@@ -15,19 +16,23 @@
 #' summary(as_flat(x))
 NULL
 
-#' @describeIn fsm Automatically convert to either a `kfsm` or a `fsm`.
-#' + If `x` is a list of list of equal-sized matrices then it returns
-#' `x` with class `ms` added.
-#' If x is a list of symmetric matrices then it will become an `TFORGE_fsm`.
-#' In the rare case that `x` is a list, and each element is a matrix *vectorised* matrices such that each element of `x` is symmetric then `as_flat()` will mistakenly treat each each element of `x` as a symmetric tensor and return an `TFORGE_fsm` object.
-#' @param x An object. 
-#' @param ... Passed to `isSymmetric()` for testing whether matrices are symmetric.
+#' @describeIn fsm Automatically convert to either a `TFORGE_kfsm` or a `TFORGE_fsm`.
+#' @details
+#' The matrices inside `x` must all have the same dimension.
+#' 
+#' The function `as_flat()` automatically chooses between a `TFORGE_kfsm` or a `TFORGE_fsm`:
+#' + If `x` is a list of symmetric matrices then it will return a `TFORGE_fsm`.
+#' + If `x` is a list of lists of equal-sized matrices then it returns a `TFORGE_kfsm`, with each element of the larger list a `TFORGE_fsm`.
+#' + If `x` is a list of 2D arrays, each satisfying `as_fsm()`, then `as_flat()` will return a `TFORGE_kfsm`.
+#' + In the rare case that `x` is a list of 2D arrays of flattened matrices, but the 2D arrays happen to be perfectly symmetric (requires size of collections to perfectly relate to the size of the matrix observations) then `as_flat()` will mistakenly treat each element of `x` as a symmetric matrix and return a `TFORGE_fsm`.
+#' @param x For `as_fsm()` a list of symmetric matrices, or a 2D array of flattened matrices. For `as_kfsm()` a list of objects that can be passed to `as_fsm()` (i.e. a list of a lists of matrices, or a list of 2D arrays). For `as_flat()`, `x` can be suitable for either `as_fsm()` or `as_kfsm()`.
+#' @param ... Passed to [`isSymmetric()`] for testing whether matrices are symmetric.
 #' @return An object with class `TFORGE_kfsm` or `TFORGE_fsm`.
 #' @export
 as_flat <- function(x, ...){
   if (inherits(x, "TFORGE_kfsm")){return(x)}  #isa() requires a match on all elements of the class attribute, so inherits() more suitable
   if (inherits(x, "TFORGE_fsm")){return(x)}
-  if (inherits(x, "matrix")){return(as_fsm(x))}
+  if (inherits(x, "array")){return(as_fsm(x))}
 
   return(as_kfsm(x, ...))
 }
@@ -51,11 +56,10 @@ as_kfsm <- function(x, ...){
 }
 
 #' @describeIn fsm For `x` a list of symmetric matrices of the same size, flattens `x` into a 2D array the `i`th row is a flattened version `vech(x[[i]])` of the `i`th matrix of  `x`. If `x` is already flattened then `as_fsm()` will check that the number of columns are consistent with a flattened symmetric matrix.
-#' @param ... is passed to [`isSymmetric()`] for checking symmetric matrices.
 #' @export
 as_fsm <- function(x, ...){
   if (inherits(x, "TFORGE_fsm")){return(x)}
-  if (inherits(x, "matrix")){
+  if (inherits(x, "array")){
     # check that columns numbers make sense
     tryCatch(dimfromvech(x[1, ]),
              error = function(e){
@@ -76,7 +80,7 @@ as_fsm <- function(x, ...){
   if (!all(vapply(x, isSymmetric, FUN.VALUE = FALSE, ...))){stop("Some matrices are not symmetric according to default limits in isSymmetric().")}
   xvec <- lapply(x, vech)
   xmat <- do.call(rbind, xvec)
-  class(xmat) <- c("TFORGE_fsm", class(xmat))
+  class(xmat) <- c("TFORGE_fsm", setdiff(class(xmat), "matrix"))
   return(xmat)
 }
 
