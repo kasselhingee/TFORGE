477269688a3dabc25173ae296e4b715c5dcd0719
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-06-21 09:38:36 +1000

    rsymm_t argument changed to mean

--- R/rsymm_t.R
+++ R/rsymm_t.R
@@ -3,33 +3,34 @@
 #' Simulate symmetric matrices with elements from a multivariate t distribution.
 #' @param sigma The scale paramater matrix for the elements arranged by [`vech()`]. `sigma` is passed to [`mvtnorm::rmvt()`] without any transformation.
 #' @return A `TFORGE_fsm` object. See [`as_fsm()`].
-#' @details Using the use Lin 1972 representation A to simulate about mean of zero. Then shift all simulated values by delta. The covariance of the result is sigma *  df / (df - 2).
 #' @examples 
-#' Ys <- rsymm_t(100, delta = matrix(1, nrow = 3, ncol = 3), df = 10, sigma = diag(c(3,2,1,1,1,1)))
-#' @export
+#' Ys <- rsymm_t(100, mean = matrix(1, nrow = 3, ncol = 3), df = 10, sigma = diag(c(3,2,1,1,1,1)))
 #' @param n Number of matrices to generate.
 #' @param mean A symmetric matrix specifying the mean of the distribution.
 #' @param df Degrees of freedom for the t distribution.
 #' 
 #' @details 
-#' The function uses Representation A in \insertCite{lin1972ch}{TFORGE} to simulate multivariate-t vectors. It matrices around a mean of zero and then shifts all simulated values by `delta`.
-#' The mean matrix `delta` is vectorized using the [`vech()`] function. The covariance of the resulting matrices is given by `sigma * df / (df - 2)`.
+#' The function uses Representation A in \insertCite{lin1972ch}{TFORGE} to simulate multivariate-t vectors.
+#' The covariance of the vectors is `sigma * df / (df - 2)`.
+#' The mean matrix `mean` is vectorized using the [`vech()`] function.
 #' 
 #' @examples 
-#' # Generate 100 symmetric matrices with delta as a matrix of ones and specified degrees of freedom
-#' Ys <- rsymm_t(100, delta = matrix(1, nrow = 3, ncol = 3), df = 10, sigma = diag(c(3,2,1,1,1,1)))
+#' rsymm_t(100, mean = matrix(1, nrow = 3, ncol = 3), df = 10, sigma = diag(c(3,2,1,1,1,1)))
 #' 
-rsymm_t <- function(n, delta, df = 1, sigma = diag(length(vech(delta)))){
-  stopifnot(isSymmetric(delta))
-  delta <- vech(delta, name = TRUE)
-  zeros <- rep(0, length(delta))
+#' @references \insertAllCited{}
+#' @export
+rsymm_t <- function(n, mean, df = 1, sigma = diag(length(vech(mean)))){
+  stopifnot(inherits(mean, "matrix"))
+  stopifnot(isSymmetric(mean))
+  mean <- vech(mean, name = TRUE)
+  zeros <- rep(0, length(mean))
   nuS2 <- rchisq(n, df)
   S2 <- nuS2/df
   sim_l <- lapply(S2, function(x){
     mvtnorm::rmvnorm(n = 1, mean = zeros, sigma = sigma / x)
   })
   out <- do.call(rbind, sim_l)
-  out <- t(t(out) + delta)
+  out <- t(t(out) + mean)
   colnames(out) <- names(mean)
   class(out) <- c("TFORGE_fsm", class(out))
   return(out)
--- tests/testthat/test-rsymm_t.R
+++ tests/testthat/test-rsymm_t.R
@@ -1,7 +1,7 @@
 test_that("rsymm_t has correct mean", {
   set.seed(3654)
   sigma <- diag(c(3,2,1,1,1,1))
-  Ys <- rsymm_t(1E4, delta = matrix(1, nrow = 3, ncol = 3), df = 10, sigma = sigma)
+  Ys <- rsymm_t(1E4, mean = matrix(1, nrow = 3, ncol = 3), df = 10, sigma = sigma)
   expect_lt(max(abs(colMeans(Ys) - 1)), 2E-1)
   expect_equal(cov(Ys), sigma * 10/(10-2), 1E-1)
   # do.call(rbind, lapply(Ys, vech, name = TRUE)) |>
