3a0eb544ad8cff5c97831a8825466fd0824b5657
Kassel Hingee <kassel.hingee@anu.edu.au>
2024-06-21 11:24:39 +1000

    test was comparing dimnames

--- tests/testthat/test-rsymm_t.R
+++ tests/testthat/test-rsymm_t.R
@@ -3,7 +3,9 @@ test_that("rsymm_t has correct mean", {
   sigma <- diag(c(3,2,1,1,1,1))
   Ys <- rsymm_t(1E4, mean = matrix(1, nrow = 3, ncol = 3), df = 10, sigma = sigma)
   expect_lt(max(abs(colMeans(Ys) - 1)), 2E-1)
-  expect_equal(cov(Ys), sigma * 10/(10-2), 1E-1)
+  simcov <- cov(Ys)
+  dimnames(simcov) <- NULL
+  expect_equal(simcov, sigma * 10/(10-2), 1E-1)
   # do.call(rbind, lapply(Ys, vech, name = TRUE)) |>
   #   data.frame() |>
   #   tidyr::pivot_longer(everything()) |>
