a9b9d43e1f7d802a9ad796e5abd112ebd67c94eb
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-01-24 19:18:39 +1100

    cleaned up project refbasis

--- tests/testthat/test-multiplicity-refbasis.R
+++ tests/testthat/test-multiplicity-refbasis.R
@@ -1,5 +1,5 @@
 test_that("project_basis independent of subspace basis", {
-  p <- 4
+  p <- 5
   # Reference basis:
   # set.seed(1)
   refbasis <- mclust::randomOrthogonalMatrix(p, p)
@@ -15,21 +15,21 @@ test_that("project_basis independent of subspace basis", {
   # apply rotation to q_abasis
   q_abasis2 <- q_abasis %*% R
   bas2 <- project_basis(q_abasis2, refbasis)
-  expect_equal(bas1, bas2)
+  expect_equal(abs(t(bas1) %*% bas2), diag(nrow = q))
 })
 
 test_that("project_basis onto full space preserves ref", {
-  p <- 3
+  p <- 4
   # Reference basis:
   set.seed(1)
   refbasis <- mclust::randomOrthogonalMatrix(p, p)
   q_abasis <- mclust::randomOrthogonalMatrix(p, p)
   bas1 <- project_basis(q_abasis, refbasis)
-  expect_equal(bas1, refbasis)
+  expect_equal(abs(t(bas1) %*% refbasis), diag(nrow = p))
 })
 
 test_that("project_basis depends on reference", {
-  p <- 3
+  p <- 5
   q <- 2
   # Reference basis:
   set.seed(1)
@@ -38,30 +38,17 @@ test_that("project_basis depends on reference", {
   q_abasis <- mclust::randomOrthogonalMatrix(p, p)[, 1:q]
   bas1 <- project_basis(q_abasis, refbasis)
   bas2 <- project_basis(q_abasis, refbasis2)
-  expect_error(expect_equal(bas1, bas2))
+  expect_error(expect_equal(abs(t(bas1) %*% bas2), diag(nrow = q), tolerance = 0.1))
 })
 
-# project ref basis to eigenspace
-projbasis1 <- q_abasis %*% t(q_abasis) %*% refbasis
-svd(projbasis1)
-
-# rotation in eigenspace
-set.seed(2)
-R <-  mclust::randomOrthogonalMatrix(q, q)
-# apply rotation to q_abasis
-q_abasis2 <- q_abasis %*% R
-projbasis2 <- q_abasis2 %*% t(q_abasis2) %*% refbasis
-all.equal(projbasis1, projbasis2)
-all.equal(q_abasis2 %*% t(q_abasis2), q_abasis %*% t(q_abasis)) #projection matrices for a space don't care about how subspace is parameterised.
-# Therefore using the SVD/eigenvectors of the projection matrix is a fine thing to do in the sense that they are independent of the parameterisation of the space.
-# But I think it is only looking stable because of the computation accuracy
-
-projmat <- q_abasis %*% t(q_abasis)
-all.equal(svd(projmat), svd(projmat %*% refbasis))
-alignedsvd <- svd(projmat %*% diag(1 + (p:1)/10000) %*% refbasis, nu = q, nv = q)
-all.equal(alignedsvd$u %*% t(alignedsvd$u), projmat)
-# Fails: all.equal(alignedsvd$v %*% t(alignedsvd$v), projmat)
-
-# alignedsvd$u are candidate axes for the subspace.
-
+test_that("project_basis returns vector for 1d subspace", {
+  p <- 3
+  q <- 1
+  # Reference basis:
+  set.seed(1)
+  refbasis <- mclust::randomOrthogonalMatrix(p, p)
+  q_abasis <- mclust::randomOrthogonalMatrix(p, p)[, 1:q, drop = FALSE]
+  bas1 <- project_basis(q_abasis, refbasis)
+  expect_equal(drop(t(bas1) %*% q_abasis), 1)
+})
 
