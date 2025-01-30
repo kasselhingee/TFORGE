2d97c0244d65e3d0f62e9d04d59afd1dc8fbbf6f
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-01-30 16:10:55 +1100

    cov_evals test: removed dev stuff on mincorr

--- tests/testthat/test-cov_evals.R
+++ tests/testthat/test-cov_evals.R
@@ -23,20 +23,3 @@ test_that("cov_evals() and cov_evals2() match", {
   expect_equal(cov_evals(evecs, mcov), cov_evals2(evecs, mcov))
 })
 
-ssqoffdiagonal <- function(vec, mcov){
-  A <- matrix(0, 3, 3)
-  A[lower.tri(A)] <- vec
-  A[upper.tri(A)] <- -t(A)[upper.tri(A)]
-  trialevecs <- sphm:::cayleyTransform(A)
-  evalcov <- cov_evals2(trialevecs, mcov)
-  sum(evalcov[lower.tri(A)]^2)
-}
-best <- optim(par = sphm:::inverseCayleyTransform(diag(1, 3))[lower.tri(diag(1, 3))],
-      fn = ssqoffdiagonal,
-      mcov = covS
-      )
-A <- matrix(0, 3, 3)
-A[lower.tri(A)] <- best$par
-A[upper.tri(A)] <- -t(A)[upper.tri(A)]
-bestevecs <- sphm:::cayleyTransform(A)
-cov_evals(bestevecs, covS)
