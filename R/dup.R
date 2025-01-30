73de467b07fc060602e9715bd3e7ec2d36c09260
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-01-30 16:53:05 +1100

    memoisation of dup matrix

--- R/dup.R
+++ R/dup.R
@@ -2,8 +2,11 @@
 #' @title Duplication matrix
 #' @description Duplication matrix as defined by \insertCite{@Section 3.8 @magnus2019ma}{TFORGE}
 #' @details The entries of the matrix are inferred from the indexes using [`which()`], so creation of the matrix can be computationally intensive for large `n`.
+#' 
+#' The function is memoised so calling dup on the same n does not take any more time.
 
-dup <- function(n){
+
+dup_direct <- function(n){
   x <- 1:(n*(n+1)/2)
   m <- invvech(x)
   y <- vec(m)
@@ -13,3 +16,20 @@ dup <- function(n){
   return(out)
 }
 
+# function f must have atomic inputs only
+memoise_simple_function <- function(f) {
+  cache <- new.env(parent = emptyenv())  # Create an isolated environment
+  
+  function(...) {
+    key <- paste(..., collapse = "_")  # Simple key (works for atomic inputs)
+    if (exists(key, envir = cache)) {
+      return(get(key, envir = cache))  # Return cached result
+    }
+    
+    result <- f(...)  # Compute function
+    assign(key, result, envir = cache)  # Store result in cache
+    return(result)
+  }
+}
+
+dup <- memoise_simple_function(dup_direct)
--- tests/testthat/test-dup.R
+++ tests/testthat/test-dup.R
@@ -2,3 +2,10 @@ test_that("dup() gives matrix that works on vec() and vech() results", {
   m <- matrix(c(1,2,3,2,5,6,3,6,9), byrow = FALSE, nrow = 3)
   expect_equal(drop(dup(3) %*% vech(m)), vec(m))
 })
+
+test_that("dup() memoisation works", {
+  dup1 <- dup_direct(10)
+  expect_equal(dup(10), dup1)
+  time1 <- system.time(dup_direct(50))
+  expect_lt(system.time(dup(50))["user.self"], time1["user.self"])
+})
