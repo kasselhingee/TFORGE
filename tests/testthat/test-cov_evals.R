8ce243a26a69ce75712a0c0af2b8e85a1cfff00f
Kassel Hingee <kassel.hingee@anu.edu.au>
2025-03-24 15:22:51 +1100

    obsolete test of cov_evals_inside

--- tests/testthat/test-cov_evals.R
+++ tests/testthat/test-cov_evals.R
@@ -1,13 +1,3 @@
-test_that("cov_evals_inside() matches cov_evals_inside_cpp()",{
-  set.seed(12)
-  Ysample <- as_fsm(rsymm(1E2, diag(c(3,2,1))))
-  mcov <- mcovar(Ysample)
-  evecs <- eigen_desc(mmean(Ysample))$vectors
-  dupmat <- dup(nrow(evecs))
-  expect_equal(cov_evals_inside(evecs[, 1], evecs[, 2], dupmat, mcov), 
-    cov_evals_inside_cpp(evecs[, 1], evecs[, 2], dupmat, mcov))
-})
-
 test_that("cov_evals() and cov_evals_old() match", {
   set.seed(12)
   Ysample <- as_fsm(rsymm(1E2, diag(c(3,2,1))))
